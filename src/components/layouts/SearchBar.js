07ba9094a9a051ed932dc7527af0b0f307b8a61a
Kafui Alordo <kalordo7@gmail.com>
2022-05-17 12:45:41 +0000

    Searching Logs via Redux Actions

--- src/components/layouts/SearchBar.js
+++ src/components/layouts/SearchBar.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {connect} from 'react-redux';
+import {searchLogs} from '../../actions/logActions';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => {
+const SearchBar = ({searchLogs}) => {
+    const text = useRef();
+
+    const onChange = () => {
+        searchLogs(text.current.value);
+    };
+
     return (
         <div>
             <nav style={{marginBottom: '30px'}} className='blue'>
                 <div className='nav-wrapper'>
                     <form>
                         <div className='input-field'>
-                            <input id='search' type='search' required />
+                            <input
+                                id='search'
+                                type='search'
+                                placeholder='Search Logs here'
+                                ref={text}
+                                onChange={onChange}
+                                required
+                            />
                             <label className='label-icon' htmlFor='search'>
                                 <i className='material-icons'>search</i>
                             </label>
@@ -20,4 +36,8 @@ const SearchBar = () => {
     );
 };
 
-export default SearchBar;
+SearchBar.propTypes = {
+    searchLogs: PropTypes.func.isRequired,
+};
+
+export default connect(null, {searchLogs})(SearchBar);

e17de2ae11f8455b30513ae35a5be8a37f9d3e44
Kafui Alordo <kalordo7@gmail.com>
2022-05-17 11:35:44 +0000

    Deleting Logs via Redux Actions

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG} from './types';
+import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG} from './types';
 
 export const getLogs = (log) => {
     return async (dispatch) => {
@@ -40,6 +40,29 @@ export const addLog = (log) => {
             dispatch({
                 type: ADD_LOG,
                 payload: data,
+            });
+        } catch (error) {
+            dispatch({
+                type: LOGS_ERROR,
+                payload: error.response.data,
+            });
+        }
+    };
+};
+
+// Delete Log from Server
+export const deleteLog = (id) => {
+    return async (dispatch) => {
+        try {
+            setLoading();
+
+            await fetch(`/logs/${id}`, {
+                method: 'DELETE',
+            });
+
+            dispatch({
+                type: DELETE_LOG,
+                payload: id,
             });
         } catch (error) {
             dispatch({
